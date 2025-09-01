d831a1969d1c865131a56dfc859e2db48e0fc96a
moonwatcher432 <moonwatcher432@gmail.com>
2025-09-01 17:58:42 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from tools import calculator
+
+
+def main():
+    st.set_page_config(page_title="Debate Tools", layout="wide")
+    
+    st.title("gay")
+    hello = "AAAA"
+    f'''
+    
+    This is a page. 
+    # {hello}
+    
+    '''
+    
+    tool_out = st.write("")
+    # Sidebar navigation
+    tool_choice = st.sidebar.selectbox(
+        "Choose a tool:",
+        ["Tool 1", "Tool 2"]
+    )
+    
+    if tool_choice == "Tool 1":
+        result = calculator.run(1, 2)
+
+
+if __name__ == "__main__":
+    main()
+
+    
