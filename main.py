1e850b089e706b1862969d8e11a0cf77fe1f830c
moonwatcher432 <moonwatcher432@gmail.com>
2025-09-02 19:24:18 +0000

    Refactor main.py to update tool link display logic and improve code clarity

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ st.title(":red[Debate] Tools")
 st.write("made by logan madler")
 st.write("\n")
 st.write("Current list of tools:")
+
 # Print tool list
 for i, (tool, is_available) in enumerate(tools.items(), start=1):
     status = "Available" if is_available else "Unavailable"
     
     st.write(f"{i}. {tool.capitalize()}: {status}")
-    st.link_button(tool.capitalize(), f"https://debatetools.streamlit.app/{tool}")
+    #st.link_button(tool.capitalize(), f"https://debatetools.streamlit.app/{tool}")
+    st.page_link(f"{tool}.py", label=tool.capitalize())
 
 
     
