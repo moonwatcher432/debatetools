0a63c8a32bdc68c0d7e2ff13060999c9b30a7677
moonwatcher432 <moonwatcher432@gmail.com>
2025-09-02 05:01:43 +0000

    Refactor main.py to simplify the main webpage format and remove unused code

--- main.py
+++ main.py
@@ -4,24 +4,11 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="Debate Tools", layout="wide")
     
-    st.title("gay")
-    hello = "AAAA"
-    f'''
+    # Main webpage format
+    st.markdown(f'''**This is a bolded title*
     
-    This is a page. 
-    # {hello}
+    ''')
     
-    '''
-    
-    tool_out = st.write("")
-    # Sidebar navigation
-    tool_choice = st.sidebar.selectbox(
-        "Choose a tool:",
-        ["Tool 1", "Tool 2"]
-    )
-    
-    # if tool_choice == "Tool 1":
-    #     result = calculator.run(1, 2)
 
 
 if __name__ == "__main__":
